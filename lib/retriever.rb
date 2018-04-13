b1c25f589d4715151ddbaed08478a21e93f8fb44
Marco Zanini <chetto87@gmail.com>
2018-04-12 20:33:00 -0700

    Retrieve stock info for a day

--- lib/retriever.rb
+++ lib/retriever.rb
@@ -1,9 +1,16 @@
+require 'rest-client'
+
 class Retriever
 
   attr_accessor :token
 
   def initialize(token)
     @token = token
+  end
+
+  def stock(name, date) 
+    response = RestClient.get "https://www.quandl.com/api/v3/datatables/WIKI/PRICES.json?ticker=#{name}&date=#{date}&api_key=#{token}"
+    return response.body
   end
 
 end
--- spec/retriever_spec.rb
+++ spec/retriever_spec.rb
@@ -1,11 +1,25 @@
+require 'json'
 require_relative '../lib/retriever'
 
 describe Retriever do 
+
+  before :all do
+    token = File.open('token', 'r').read
+    @retriever = Retriever.new(token);
+  end
+
   describe '#initialize' do
-    it 'is initialized with the token' do 
-      token = File.open('token', 'r').read
-      retriever = Retriever.new(token);
-      expect(retriever.token).to eq(token)  
+    it 'is initialized with the token' do
+      token = File.open('token', 'r').read 
+      expect(@retriever.token).to eq(token)  
+    end
+  end
+
+  describe '#stock' do 
+    it 'retrieves all data for the specified security for that day' do
+      stockInfo = @retriever.stock('MSFT', '2017-06-01')
+      info = JSON.parse(stockInfo)
+      expect(info['datatable']['data'][0][0]).to eq('MSFT')
     end
   end
 
