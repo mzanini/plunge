e5f2ca5708a30ac611ec02c29058a0e4d1a7a2fd
Marco Zanini <chetto87@gmail.com>
2018-04-15 21:35:19 -0700

    Create common method to call api

--- lib/retriever.rb
+++ lib/retriever.rb
@@ -16,24 +16,16 @@ class Retriever
 
   def stock(name, date) 
     log.info "Retrieving stock info for stock: #{name} at date: #{date}"
-    begin
-      response = RestClient.get "#{@base_url}ticker=#{name}&date=#{date.strftime('%Y-%m-%d')}&api_key=#{token}"
-    rescue RestClient::ExceptionWithResponse => e
-      e.response
-    end
+    response = query_api(name, date)
     
-    return response.body
+    return response
   end
 
   def volume(name, date) 
     log.info "Retrieving volume info for stock: #{name} at date: #{date}"
-    begin
-      response = RestClient.get "#{@base_url}ticker=#{name}&date=#{date.strftime('%Y-%m-%d')}&api_key=#{token}&qopts.columns=volume"
-    rescue RestClient::ExceptionWithResponse => e
-      e.response
-    end
-    
-    data = JSON.parse(response.body)
+    response = query_api(name, date, 'volume')
+
+    data = JSON.parse(response)
     begin
       return data['datatable']['data'][0][0]
     rescue
@@ -43,13 +35,9 @@ class Retriever
 
   def opening(name, date)
     log.info "Retrieving stock opening price for stock: #{name} at date: #{date}"
-    begin
-      response = RestClient.get "#{@base_url}qopts.columns=open&ticker=#{name}&date=#{date.strftime('%Y-%m-%d')}&api_key=#{token}"
-    rescue RestClient::ExceptionWithResponse => e
-      e.response
-    end
+    response = query_api(name, date, 'open')
     
-    data = JSON.parse(response.body)
+    data = JSON.parse(response)
     begin
       return data['datatable']['data'][0][0]
     rescue
@@ -59,13 +47,9 @@ class Retriever
 
   def closing(name, date)
     log.info "Retrieving stock closing price for stock: #{name} at date: #{date}"
-    begin
-      response = RestClient.get "#{@base_url}qopts.columns=close&ticker=#{name}&date=#{date.strftime('%Y-%m-%d')}&api_key=#{token}"
-    rescue RestClient::ExceptionWithResponse => e
-      e.response
-    end
+    response = query_api(name, date, 'close')
     
-    data = JSON.parse(response.body)
+    data = JSON.parse(response)
     begin
       return data['datatable']['data'][0][0]
     rescue
@@ -75,13 +59,9 @@ class Retriever
 
   def opening_and_closing(name, date) 
     log.info "Retrieving opening and closing price for stock: #{name} at date: #{date}"
-    begin
-      response = RestClient.get "#{@base_url}qopts.columns=open,close&ticker=#{name}&date=#{date.strftime('%Y-%m-%d')}&api_key=#{token}"
-    rescue RestClient::ExceptionWithResponse => e
-      e.response
-    end
+    response = query_api(name, date, 'open,close')
     
-    data = JSON.parse(response.body)
+    data = JSON.parse(response)
     begin
       return data['datatable']['data'][0][0], data['datatable']['data'][0][1]
     rescue
@@ -91,13 +71,9 @@ class Retriever
 
   def high_and_low(name, date) 
     log.info "Retrieving opening and closing price for stock: #{name} at date: #{date}"
-    begin
-      response = RestClient.get "#{@base_url}qopts.columns=high,low&ticker=#{name}&date=#{date.strftime('%Y-%m-%d')}&api_key=#{token}"
-    rescue RestClient::ExceptionWithResponse => e
-      e.response
-    end
+    response = query_api(name, date, 'high,low')
     
-    data = JSON.parse(response.body)
+    data = JSON.parse(response)
     begin
       return data['datatable']['data'][0][0], data['datatable']['data'][0][1]
     rescue
@@ -105,4 +81,21 @@ class Retriever
     end
   end
 
+  def query_api(stock, date, columns = nil)
+    if not columns.nil?
+      colums_selector = "qopts.columns=#{columns}"    
+    else
+      colums_selector = ''
+    end 
+
+    begin
+      response = RestClient.get "#{@base_url}ticker=#{stock}&api_key=#{token}&date=#{date.strftime('%Y-%m-%d')}&#{colums_selector}"
+    rescue RestClient::ExceptionWithResponse => e
+      e.response
+    end
+    
+    return response.body
+  end
+
+  private :query_api
 end
